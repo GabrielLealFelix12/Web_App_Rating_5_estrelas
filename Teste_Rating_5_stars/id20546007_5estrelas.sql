cc404a4235cf74dceb30793ec87ae33a3cadcf60
Gabriel Leal Felix <75867684+GabrielLealFelix12@users.noreply.github.com>
2023-09-17 10:31:58 -0400

    Update id20546007_5estrelas.sql

--- Teste_Rating_5_stars/id20546007_5estrelas.sql
+++ Teste_Rating_5_stars/id20546007_5estrelas.sql
@@ -21,7 +21,7 @@ CREATE TABLE `reviews` (
   `USERPFP` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `COMENTARIO` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `ESTRELAS` int(1) NOT NULL,
-  `id` int(255) NOT NULL DEFAULT current_timestamp()
+  `id` bigint(255) NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
